76b077fee3ee292014750b64a2920219b207dd6e
Rui Wang-Dong <rtdong9421@gmail.com>
2020-09-23 18:26:23 -0400

    Updates

---
+++ precache-manifest.9098a5e9ddd0f745f9c2b0ea2f737733.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c586c399cc325ef1bfd929bfda32674",
+    "url": "/my-js-expo/index.html"
+  },
+  {
+    "revision": "d540684db796029d75c4",
+    "url": "/my-js-expo/static/css/main.82880db2.chunk.css"
+  },
+  {
+    "revision": "b7cde4099d0960d702a6",
+    "url": "/my-js-expo/static/js/2.be5b8035.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-js-expo/static/js/2.be5b8035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d540684db796029d75c4",
+    "url": "/my-js-expo/static/js/main.99d6af43.chunk.js"
+  },
+  {
+    "revision": "34b8a35f3ec804d19757",
+    "url": "/my-js-expo/static/js/runtime-main.10b0ed85.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/my-js-expo/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "b6aa471c3d30ed934ad4c304de1e71c8",
+    "url": "/my-js-expo/static/media/bulletin.b6aa471c.jpg"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/my-js-expo/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/my-js-expo/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/my-js-expo/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/my-js-expo/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "97378a3d448c6ba562b7851618a6de21",
+    "url": "/my-js-expo/static/media/polaroidClock.97378a3d.png"
+  },
+  {
+    "revision": "68910ce1c31b8bd5a0fd5ee3af5a9cec",
+    "url": "/my-js-expo/static/media/polaroidDrumKit.68910ce1.png"
+  },
+  {
+    "revision": "d4dcf8e7dbe120e81e54b82088f061f5",
+    "url": "/my-js-expo/static/media/postIt.d4dcf8e7.png"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/my-js-expo/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/my-js-expo/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "35f0ce1f4e5e04b583f9b3bf06b1d480",
+    "url": "/my-js-expo/static/media/tape.35f0ce1f.png"
+  },
+  {
+    "revision": "75f727ec5b83f099bdac79c49edca79a",
+    "url": "/my-js-expo/static/media/tink.75f727ec.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/my-js-expo/static/media/tom.572ed39d.wav"
+  }
+]);
